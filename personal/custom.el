10345ac4c62ce2a2a074ff4c537cf7739618327a
Zvonko Kosic <zvonkok@gmail.com>
2016-03-11 13:25:42 +0100

    Added personal custom and init.el zonk

--- personal/custom.el
+++ personal/custom.el
@@ -5,6 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("b61c55259c639a54628f91452b060b99c550a1269eb947e372321b806b68f114" "e87a2bd5abc8448f8676365692e908b709b93f2d3869c42a4371223aab7d9cf8" "0788bfa0a0d0471984de6d367bb2358c49b25e393344d2a531e779b6cec260c5" "fbcdb6b7890d0ec1708fa21ab08eb0cc16a8b7611bb6517b722eba3891dfc9dd" "beeb5ac6b65fcccfe434071d4624ff0308b5968bf2f0c01b567d212bcaf66054" "8f0334c430540bf45dbcbc06184a2e8cb01145f0ae1027ce6b1c40876144c0c9" "8e7ca85479dab486e15e0119f2948ba7ffcaa0ef161b3facb8103fb06f93b428" default)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(size-indication-mode t)
--- personal/zonk.el
+++ personal/zonk.el
@@ -1,4 +1,19 @@
 (prelude-require-packages '(org))
 
-
 (require 'prelude-helm-everywhere)
+
+
+(require 'powerline)
+
+
+(setq airline-utf-glyph-separator-left      #xe0b0
+      airline-utf-glyph-separator-right     #xe0b2
+      airline-utf-glyph-subseparator-left   #xe0b1
+      airline-utf-glyph-subseparator-right  #xe0b3
+      airline-utf-glyph-branch              #xe0a0
+      airline-utf-glyph-readonly            #xe0a2
+      airline-utf-glyph-linenumber          #xe0a1)
+
+
+(require 'airline-themes)
+(load-theme 'airline-distinguished)
